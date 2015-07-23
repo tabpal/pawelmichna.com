1c4183d50bd120ac009f38757c1a06f5323c12a1
tabpal <tabpal@users.noreply.github.com>
2015-07-23 18:58:38 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Paweł Michna - THE Homepage
+		</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8" />
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen" type="text/css">
+		<link href="css/custom.css" rel="stylesheet" media="screen" type="text/css">
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="../../assets/js/html5shiv.js"></script>
+		<script src="../../assets/js/respond.min.js"></script>
+		<![endif]-->
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript">
+</script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<div class="row">
+					<div class="col-xs-5">
+						<h1>
+							Paweł Michna<br>
+							<small>Software developer</small>
+						</h1>
+					</div>
+					<div class="col-xs-7">
+						<img src="img/avatar.jpeg" class="img-circle img-responsive " />
+					</div>
+				</div>
+			</div>				
+			<div class="jumbotron">
+				<div class="row text-center">
+					<div class="col-sm-3"><a class="custom-hover" href="https://github.com/pmichna"><i class="icon-github-alt icon-5x"></i></a></div>
+					<div class="col-sm-3"><a class="custom-hover" href="http://pl.linkedin.com/pub/pawe%C5%82-michna/4b/17a/502/"><i class="icon-linkedin icon-5x"></i></a></div>
+					<div class="col-sm-3"><a class="custom-hover" href="https://twitter.com/PawelMichna"><i class="icon-twitter icon-5x"></i></a></div>
+					<div class="col-sm-3"><a class="custom-hover" href="mailto:pmichna+homepage@gmail.com"><i class="icon-envelope icon-5x"></i></a></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
